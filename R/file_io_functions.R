5f69f8a85214aaf07e14b6b20cc57dbba7681302
Nat Henry <nat.henry7@gmail.com>
2021-02-05 17:37:07 -0800

    Optionally subset datasets by administrative level.

--- R/file_io_functions.R
+++ R/file_io_functions.R
@@ -14,6 +14,9 @@
 #'
 #' @param table_name [char] Name of the CRVS table that is being loaded
 #' @param file [char] Filepath to read from
+#' @param level [integer, default NULL] Optional argument to only pull a single level of
+#'   administrative data from a location table. This can be useful when data is only being
+#'   modeled at a particular administrative level.
 #' @param ... Other arguments to data.table::fread()
 #'
 #' @return Data.table containing the table with properly formatted columns, where known
@@ -24,5 +27,18 @@ load_location_table <- function(table_name, file, ...){
   # Get possible location table columns and data types
   colClasses <- mapcrvs::get_location_table_field_classes(table_name)
   out_table <- data.table::fread(file = file, colClasses = colClasses, ...)
+
+  # Optionally subset to a particular administrative level
+  if(length(level) > 0){
+    subset_levels <- level
+    if('level' %in% colnames(out_table)){
+      out_table <- out_table[level %in% subset_levels, ]
+    } else {
+      stop("Cannot subset by admin level: 'level' is not a field in the input data.")
+    }
+  }
+
+  # Warn if this function is returning an empty table
+  if(nrow(out_table) == 0) warning("Output location table has zero rows.")
   return(out_table)
 }
